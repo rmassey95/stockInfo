c942a3ac10b0657870686d135a4be377dfe4c2be
Ryan Massey <rmassey.95@gmail.com>
2021-04-11 00:06:18 -0400

    Add starter code. Add connection to MongoDB and connection to localhost. Start creating search page

---
+++ app.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const path = require('path');
+const app = express();
+
+//connect to MongoDB
+mongoose.connect('mongodb://localhost/financeDB', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+})
+    .then(() => {
+        console.log("MONGO DB CONNECTED");
+    })
+    .catch((e) => {
+        console.log(e);
+        console.log('NO CONNECTION');
+    })
+
+//set view engine to ejs
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+
+app.get('/search', (req, res) => {
+    res.render('pages/search');
+})
+
+//create main page    
+app.get('/', (req, res) => {
+    res.send('HOME');
+})
+
+//connect to specified port
+const port = 3000;
+app.listen(port, () => {
+    console.log(`CONNECTED TO PORT ${port}`);
+});
