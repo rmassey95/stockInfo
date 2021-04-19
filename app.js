2a4b84d07b1b137c51c6edec47340ac67aee73e3
Ryan Massey <rmassey.95@gmail.com>
2021-04-18 22:32:10 -0400

    Add update feature to company page

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const mongoose = require('mongoose');
 const express = require('express');
 const path = require('path');
 const Company = require('./models/companySchema');
+const methodOverride = require('method-override');
+const { restart } = require('nodemon');
+const { findByIdAndUpdate } = require('./models/companySchema');
 
 const app = express();
 
@@ -26,6 +29,8 @@ app.set('views', path.join(__dirname, 'views'))
 
 //data parser middleware
 app.use(express.urlencoded({ extended: true }));
+//override method, allows use of put and delete for forms
+app.use(methodOverride('_method'))
 
 //company list, shows all the companies in the database
 app.get('/companies', async (req, res) => {
@@ -38,6 +43,13 @@ app.get('/company/add', (req, res) => {
     res.render('pages/companyAdd');
 })
 
+//edit company page
+app.get('/company/edit/:id', async (req, res) => {
+    const { id } = req.params;
+    const company = await Company.findById(id);
+    res.render('pages/companyEdit', { company });
+})
+
 //add company information to database
 app.post('/companies', async (req, res) => {
     const company = new Company(req.body.company);
@@ -48,10 +60,15 @@ app.post('/companies', async (req, res) => {
 //displays information on the selected company
 app.get('/company/:id', async (req, res) => {
     const { id } = req.params;
-    console.log(id);
     const company = await Company.findById(id);
-    console.log(company);
     res.render('pages/companyPage', { company });
+})
+
+//updates company with new edits
+app.put('/company/:id', async (req, res) => {
+    const { id } = req.params;
+    await Company.findByIdAndUpdate(id, req.body.company);
+    res.redirect(`/company/${id}`);
 })
 
 //create main page    
